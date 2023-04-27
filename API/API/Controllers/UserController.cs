8388cd60e235b059c8bc9c8b4a3d56775ee2085f
ntavora <ntavora@devsutd.com>
2023-04-27 10:03:52 -0300

    User controller

---
+++ API/API/Controllers/UserController.cs
@@ -0,0 +1,105 @@
+﻿using API.ViewModels;
+using AutoMapper;
+using Data.Dapper.Models;
+using Data.Dapper.Repositories;
+using Data.Dapper.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUsersRepository _usersRepository;
+        private readonly IMapper _mapper;
+        public UserController(IUsersRepository usersRepository, IMapper mapper) : base()
+        {
+            _mapper = mapper;
+            _usersRepository = usersRepository;
+        }
+       
+
+
+
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("/GetAll")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IEnumerable<UserViewModel>> Get()
+        {
+            var result = await _usersRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<UserViewModel>>(result);
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("/GetAll/{page}/{pageSize}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]        
+        public async Task<ActionResult> GetAll( int page = 1, int pageSize = 10)
+        {
+            var result = await _usersRepository.GetByDynamicAsync(new { }, page, pageSize, "CreatedDate");
+            return Ok(_mapper.Map<IEnumerable<UserViewModel>>(result));
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("/GetbyId/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetById(Guid Id)
+        {
+            Users def = new Users();
+            if (Id != Guid.Empty)
+            {
+                var result = await _usersRepository.GetAsync(Id);
+                return Ok(result);
+
+            }
+            return Ok(def);
+        }
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("/Agregar")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> Agregar(Users item)
+        {
+            item.Id = Guid.NewGuid();
+            await _usersRepository.InsertAsync(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item); ;
+        }
+        
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("/Batch/Agregar")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> BatchAdd(IEnumerable<Users> items)
+        {
+            await _usersRepository.InsertManyAsync(items);
+            return Ok(items);
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("/Actualizar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> Edit(Users item)
+        {
+            await _usersRepository.UpdateAsync(item);
+            return Ok(item);
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("/Eliminar/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> Delete(Guid Id)
+        {
+            Users def = new Users();
+            if (Id != Guid.Empty)
+            {
+                await _usersRepository.DeleteRowsByDynamicValuesAsync(new { Id });
+                return Ok(new { Mensaje = "El usuario se borro correctamente" });
+            }
+            return Ok(new { Mensaje = "Error: El usuario no existe" });
+        }
+    }
+}
