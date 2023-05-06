456cddb324c8316aa816f299b2a13e9493d42745
ntavora <ntavora@devsutd.com>
2023-05-06 18:54:11 -0300

    MMW-17 y MMW-18

---
+++ API/Data.Dapper/Migrations/20230506120004_AddServicesTable.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Dapper.Migrations
+{
+    [Migration(20230506120004)]
+    public class AddServicesTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Services")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Name").AsString(255).Unique().NotNullable()
+                .WithColumn("Description").AsString(500).NotNullable()
+                .WithColumn("CreatedDate").AsDateTime().Nullable()
+                .WithColumn("UpdatedDate").AsDateTime().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Services");
+        }
+    }
+}
---
+++ API/Data.Dapper/Models/Services.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Dapper.Models
+{
+    [Table("Services")]
+    public class Services
+    {
+        [ExplicitKey]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
